24f968a640434173ff19144ef576331d99dda7c4
Anthony Lizano <jalu_27@hotmail.com>
2023-08-21 19:02:34 -0600

    migrate social media credentials
    migrate social media credentials to database, the data is not longer storaged in the current session

---
+++ app/Models/TumblrCredential.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TumblrCredential extends Model
+{
+    protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function getByUserId($userId)
+    {
+        return self::where('user_id', $userId)->first();
+    }
+}
